4262037f00848cb93b019b5217cb3e568d582c07
Christian Catrofe <christianlopes100@outlook.com>
2024-01-18 23:32:20 -0300

    REFACTOR: Migrande de SqlAlchemy poara SQLModel. Preciso refatorar somente as queries agora.

--- src/infra/database.py
+++ src/infra/database.py
@@ -1,61 +1,47 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Any
+from typing import Any, Optional
 
-from sqlalchemy import ForeignKey
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlmodel import Field, SQLModel
 
 from src.infra.settings import Settings
 
 settings = Settings()
 
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
+
 
 def get_session_maker() -> Any:
-    url = settings.DATABASE_URL
-    engine = create_async_engine(
-        url,
-        echo=False,
-    )
     return async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def create_database() -> None:
-    url = settings.DATABASE_URL
-    engine = create_async_engine(
-        url,
-        echo=False,
-    )
-
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
-class Base(DeclarativeBase):
-    pass
+class Company(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(min_length=2, max_length=155)
+    phone: str = Field(min_length=10, max_length=20)
+    email: str = Field(min_length=8, max_length=155)
+    created_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None, nullable=True)
 
 
-class Company(Base):
-    __tablename__ = "companies"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    address: Mapped[str]
-    phone: Mapped[str]
-    email: Mapped[str]
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
-    updated_at: Mapped[datetime] = mapped_column(onupdate=datetime.now())
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str]
-    password: Mapped[str]
-    email: Mapped[str] = mapped_column(unique=True)
-    phone: Mapped[str] = mapped_column(unique=True)
-    company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"), nullable=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
-    updated_at: Mapped[datetime] = mapped_column(onupdate=datetime.now(), nullable=True)
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, min_length=3, max_length=50)
+    password: str = Field(min_length=8, max_length=155)
+    email: str = Field(unique=True, min_length=8, max_length=155)
+    phone: str = Field(unique=True, min_length=10, max_length=20)
+    company_id: Optional[int] = Field(
+        default=None, nullable=True, foreign_key="company.id"
+    )
+    created_at: Optional[datetime] = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(
+        sa_column=Column("updated_at", DateTime, onupdate=datetime.now()))
